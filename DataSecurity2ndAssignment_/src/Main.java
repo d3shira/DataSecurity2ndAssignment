3ecc0bb64f880eea845221be3b2e268721244a52
d3shira <shirarandobrava@live.com>
2023-04-15 23:16:51 +0200

    class+constructors

--- DataSecurity2ndAssignment_/src/Main.java
+++ DataSecurity2ndAssignment_/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!!!!!!!!!!!!!!");
+    }
+}
+class VigenereCipher{
+    private int[] key;
+
+    public VigenereCipher(int seed) {
+        Random rand = new Random(seed);
+        this.key = new int[100];
+        for (int i = 0; i < this.key.length; i++) {
+            this.key[i] = rand.nextInt(26);
+        }
+    }
+
+    public VigenereCipher(String seed) {
+        int sum = 0;
+        for (int i = 0; i < seed.length(); i++) {
+            sum += (int)Character.toUpperCase(seed.charAt(i));
+        }
+        Random rand = new Random(sum);
+        this.key = new int[100];
+        for (int i = 0; i < this.key.length; i++) {
+            this.key[i] = rand.nextInt(26);
+        }
     }
 }
